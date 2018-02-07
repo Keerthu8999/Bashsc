e3f6c0b66eda60a221cc941aa80ae9e22f760927
Keerthana Sunder <keerubarbie@gmail.com>
2018-02-07 14:50:29 +0530

    Add files via upload

---
+++ GFG.java
@@ -0,0 +1,46 @@
+import java.io.File;
+ 
+public class GFG 
+{
+     static void RecursivePrint(File[] arr, int level) 
+     {
+
+         for (File f : arr) 
+         {
+            for (int i = 0; i < level; i++)
+                 System.out.print("\t");
+             
+             if(f.isFile()) 
+                 System.out.println(f.getName());
+              
+             else if(f.isDirectory()) 
+             { 
+                 System.out.println("[" + f.getName() + "]");
+              
+
+                 RecursivePrint(f.listFiles(), level + 1);
+             }
+         }
+    }
+     
+ public static void main(String[] args)
+    {
+ 
+        String maindirpath = "/home/csh/Desktop/labprog";
+                 
+
+        File maindir = new File(maindirpath);
+          
+        if(maindir.exists() && maindir.isDirectory())
+        {
+
+            File arr[] = maindir.listFiles();
+             
+            System.out.println("**********************************************");
+            System.out.println("Files from main directory : " + maindir);
+            System.out.println("**********************************************");
+             
+           RecursivePrint(arr, 0); 
+       } 
+    }
+}
