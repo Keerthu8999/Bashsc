2e8edbc3ced7c5197f00d6095565eab86be4454f
Keerthana Sunder <keerubarbie@gmail.com>
2018-02-14 09:24:57 +0000

    Add files via upload

---
+++ dining.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<semaphore.h>
+#include<pthread.h>
+#define N 5
+#define THINKING 0
+#define HUNGRY 1
+#define EATING 2
+#define LEFT (ph_num+4)%N
+#define RIGHT (ph_num+1)%N
+sem_t mutex;
+sem_t S[N];
+int state[N];
+int phil_num[N]={0,1,2,3,4};
+void *philosopher(void *num)
+{
+while(1)
+{
+int *i=num;
+sleep(1);
+take_fork(*i);
+sleep(0);
+put_fork(*i);
+}}
+void take_fork(int ph_num)
+{
+sem_wait(&mutex);
+state[ph_num]=HUNGRY;
+printf("%d is hngry ",ph_num+1);
+test(ph_num);
+sem_post(&mutex);
+sem_wait(&S[ph_num]);
+sleep(1);
+}
+void test(int ph_num)
+{
+if(state[ph_num]==HUNGRY&&state[LEFT]!=EATING&&state[RIGHT]!=EATING)
+{
+state[ph_num]=EATING;
+sleep(2);
+printf("%d is eating",ph_num+1);
+sem_post(&S[ph_num]);
+}}
+void put_fork(int ph_num)
+{
+sem_wait(&mutex);
+state[ph_num]=THINKING;
+printf("%d putting fork %d  and %d done",ph_num+1,LEFT+1,RIGHT+1);
+printf("%d is thinking",ph_num=1);
+test(LEFT);
+test(RIGHT);
+sem_post(&mutex);
+}
+int main()
+{
+int i;
+pthread_t thread_id[N];
+sem_init(&mutex,0,1);
+for(i=0;i<N;i++)
+sem_init(&S[i],0,0);
+for(i=0;i<N;i++)
+{
+pthread_create(&thread_id[i],NULL,philosopher,&phil_num[i]);
+printf("Phuilospher %d is thinking \n",i+1);
+}
+for(i=0;i<N;i++)
+pthread_join(thread_id[i],NULL);
+}
---
+++ synch.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<semaphore.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/mman.h>
+sem_t mutex;
+void *thread(void* arg)
+{
+sem_wait(&mutex);
+printf("Entered");
+sleep(4);
+printf("Gonna end");
+sem_post(&mutex);
+}
+int main()
+{
+sem_init(&mutex,0,1);
+pthread_t t1,t2;
+pthread_create(&t1,NULL,thread,NULL);
+sleep(2);
+pthread_create(&t2,NULL,thread,NULL);
+pthread_join(t1,NULL);
+pthread_join(t2,NULL);
+sem_destroy(&mutex);
+return 0;
+}
